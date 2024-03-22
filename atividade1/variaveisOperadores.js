d1cfde563c2ee98166def2c32f19591059990850
LUIZ FELIPE LIMA BORGES <luiz.borges@a.ucb.br>
2024-03-22 10:10:52 -0300

    solução de atividade

---
+++ atividade1/variaveisOperadores.js
@@ -0,0 +1,15 @@
+const n1 = 2;
+const n2 = 10;
+
+let r1 = n1 + n2;
+console.log(`A soma do valores ${n1} + ${n2} = ${r1}`);
+
+r1 = n1 - n2;
+console.log(`A subtracao do valores ${n1} - ${n2} = ${r1}`);
+
+r1 = n2 - n1;
+console.log(`A subtracao do valores ${n2} - ${n1} = ${r1}`);
+
+r1 = n1 * n2;
+console.log(`A ultiplicacao do valores ${n1} X ${n2} = ${r1}`);
+
---
+++ atividade2/verificarNumeroPrimo.js
@@ -0,0 +1,5 @@
+let numero1 = 10;
+let divisor1 = 2;
+let resultado = numero1 % divisor1;
+console.log(resultado);
+if (numero1 == 2);
